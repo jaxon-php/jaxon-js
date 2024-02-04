868c5ca75355df4770abc1eccb31cf9529272a46
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2024-02-04 04:53:16 +0100

    New command to set an event handler.

--- src/cmd/event.js
+++ src/cmd/event.js
@@ -64,12 +64,31 @@
      * @param {string} command.prop The name of the event.
      * @param {string} command.func The name of the function to be called
      * @param {array} command.data The function parameters
+     * @param {object|false} command.options The handler options
      *
      * @returns {true} The operation completed successfully.
      */
-    self.addEventHandler = ({ target, prop: sEvent, func, data = [] }) => {
+    self.addEventHandler = ({ target, prop: sEvent, func, data = [], options = false }) => {
         target.addEventListener(str.stripOnPrefix(sEvent), (event) =>
-            script.call({ func, data, context: { event, target } }), false);
+            script.call({ func, data, context: { event, target } }), options);
+        return true;
+    };
+
+    /**
+     * Set an event handler with parameters to the specified target.
+     *
+     * @param {object} command The Response command object.
+     * @param {string} command.id The target element id
+     * @param {object} command.target The target element
+     * @param {string} command.prop The name of the event.
+     * @param {string} command.func The name of the function to be called
+     * @param {array} command.data The function parameters
+     *
+     * @returns {true} The operation completed successfully.
+     */
+    self.setEventHandler = ({ target, prop: sEvent, func, data = [] }) => {
+        target[str.addOnPrefix(sEvent)] = (event) =>
+            script.call({ func, data, context: { event, target } });
         return true;
     };
 })(jaxon.cmd.event, jaxon.utils.dom, jaxon.utils.string, jaxon.cmd.script);
